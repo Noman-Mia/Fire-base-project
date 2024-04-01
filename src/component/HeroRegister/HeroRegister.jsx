ef2167f349e4c7f39a2b09e74deb3219a4744cb7
Noman Mia <nomanislamnishad@gmail.com>
2024-04-01 15:56:18 +0600

    added error validation

--- src/component/HeroRegister/HeroRegister.jsx
+++ src/component/HeroRegister/HeroRegister.jsx
@@ -1,38 +1,49 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const HeroRegister = () => {
-    const [heroRegister, setHeroRegister] = useState('')
+  const [heroRegister, setHeroRegister] = useState("");
+  const [succes, setSucces] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
-    setHeroRegister("")
+    const trams = e.target.trams.checked;
+    console.log(email, password,trams);
+    //reset eror or succes
+    setHeroRegister("");
+    setSucces("");
+
+    if (password.length < 6) {
+      setHeroRegister("password should be 6 characters");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setHeroRegister("one charecter should be uppercase");
+      return;
+    } else if(!trams){
+        setHeroRegister('please accept trams and condition')
+    }
+
     //create user
-   createUserWithEmailAndPassword(auth,email,password)
-   .then(result => {
-    console.log(result.user)
-   })
-   .catch(error => {
-    console.error(error)
-    setHeroRegister(error.message)
-   })
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSucces("succesfully");
+      })
+      .catch((error) => {
+        console.error(error);
+        setHeroRegister("already in user");
+      });
   };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleRegister} className="card-body">
               <div className="form-control">
@@ -51,26 +62,33 @@ const HeroRegister = () => {
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                  name="password"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="password"
+                    className="input input-bordered"
+                    required
+                    name="password"
+                  />
+                  <span className="absolute top-4 right-8" onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                  </span>
+                </div>
+                <div>
+                    <input type="checkbox" name="trams" id="trams" />
+                    <label htmlFor="trams">Accept your <a href="">terms and condition</a></label>
+                </div>
+                
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register Now!</button>
               </div>
             </form>
-            {
-                heroRegister && <p className="text-3xl text-red-500">{heroRegister}</p>
-            }
+            {heroRegister && (
+              <p className="text-3xl text-red-500">{heroRegister}</p>
+            )}
+            {succes && <p className="text-3xl text-green-500">{succes}</p>}
+            <p>Already have an acount? please <Link to="/login">login</Link></p>
           </div>
         </div>
       </div>
