6b085a8f4ef30346b94e1eec0705793eb57fb459
Noman Mia <nomanislamnishad@gmail.com>
2024-04-01 16:54:06 +0600

    added hero register and some error fixed

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,33 +1,53 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-    const [heroRegister, setHeroRegister] = useState("");
-    const [succes, setSucces] = useState("");
-    
-    const handlelogin = e => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password); 
-        
-         //reset eror or succes
+  const [heroRegister, setHeroRegister] = useState("");
+  const [succes, setSucces] = useState("");
+  const emailRef = useRef();
+
+  const handlelogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    //reset eror or succes
     setHeroRegister("");
     setSucces("");
 
-        //add validation
-        signInWithEmailAndPassword(auth,email,password)
-        .then(result =>{
-            console.log(result.user);
-            setSucces('user log in seccesfully')
-        })
-        .catch(error => {
-            console.error(error)
-            setHeroRegister(error.message);
-        })
+    //add validation
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSucces("user log in seccesfully");
+      })
+      .catch((error) => {
+        console.error(error);
+        setHeroRegister(error.message);
+      });
+  };
+   const handleForgetPassword = () =>{
+    const email = emailRef.current.value;
+    if(!email){
+        console.log('please provide an email', emailRef.current.value);
+        return;
+    }else if(/^\+?[0-9]{1,3}?[- .]?\(?[0-9]{3}\)?[- .]?[0-9]{3}[- .]?[0-9]{4}$/.test(email)
+    ){
+        console.log("please write a valid email");
+        return
     }
+     //send validation email
+     sendPasswordResetEmail(auth,email)
+     .then(() => {
+       alert("please check your email");
+     })
+     .catch(error =>{
+        console.error(error);
+     })
+   }
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -49,6 +69,7 @@ const Login = () => {
                 <input
                   type="email"
                   name="email"
+                  ref={emailRef}
                   placeholder="email"
                   className="input input-bordered"
                   required
@@ -66,7 +87,7 @@ const Login = () => {
                   required
                 />
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
+                  <a onClick={handleForgetPassword} href="#" className="label-text-alt link link-hover">
                     Forgot password?
                   </a>
                 </label>
@@ -79,8 +100,10 @@ const Login = () => {
               <p className="text-3xl text-red-500">{heroRegister}</p>
             )}
             {succes && <p className="text-3xl text-green-500">{succes}</p>}
-            <p>New to this website? please <Link to="/heroRegister">register</Link> </p>
-           
+            <p>
+              New to this website? please{" "}
+              <Link to="/heroRegister">register</Link>{" "}
+            </p>
           </div>
         </div>
       </div>
